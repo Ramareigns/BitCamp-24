d93f47325da260374a73edf22abccc26bd7947c3
Ramith Wijesinghe <ramithwijesinghe@Ramiths-MacBook-Pro.local>
2024-04-20 17:21:06 -0400

    error 5

--- app/page.js
+++ app/page.js
@@ -25,13 +25,17 @@ export default function Home({  }) {
     <div className="flex flex-col items-center justify-center p-24 z-10">
       <h1 className="text-black text-6xl mb-10">What Would you like</h1>
       <a
-        onClick = {redirectToLoginPage}
+        onClick = {()=>redirectToLoginPage({
+          postLoginRedirectPath: window.location.href,
+      })}
         className="bg-white text-black cursor-pointer py-2 px-4 mb-5 rounded-lg hover:bg-blue-800 hover:text-white transition duration-300"
       >
         Log In
       </a>
       <a
-        onClick = {redirectToSignupPage}
+        onClick = {()=>redirectToSignupPage({
+          postLoginRedirectPath: window.location.href,
+      })}
         className="bg-white text-black py-2 px-4 cursor-pointer rounded-lg hover:bg-blue-800 hover:text-white transition duration-300"
       >
         Sign Up
