d5eb28ee038af5dde9aff2035c66c1e5c8633171
Ramith Wijesinghe <ramithwijesinghe@Ramiths-MacBook-Pro.local>
2024-04-21 10:09:48 -0400

    accout editing

--- app/Profile/page.js
+++ app/Profile/page.js
@@ -3,8 +3,16 @@
 
 import {useUser} from "@propelauth/nextjs/client"; 
 import Navbar from '../nav';
+import {useRedirectFunctions} from "@propelauth/nextjs/client"
 
 export default function Profile() {
+    const {
+        redirectToLoginPage, 
+        redirectToSignupPage, 
+        redirectToAccountPage,
+        redirectToOrgPage,
+        redirectToCreateOrgPage,
+    } = useRedirectFunctions()
 
     const {loading, user} = useUser();
       
@@ -14,22 +22,29 @@ export default function Profile() {
                 Profile
             </h1>
             <Navbar />
+            
+            
+    {
+        user && (
             <div className="rounded-full shadow-2xl bg-gray-200 w-64 h-64 flex items-center justify-center mb-4">
-                {/* Profile picture */}
-                <img src="man1.png" alt="Profile" className="rounded-full w-full h-full" />
+                <img src={user.pictureUrl} alt="Profile" className="rounded-full w-full h-full" />
             </div>
-            {/* Email and full name */}
-           {
-            user &&(
-                <div className="p-10 text-center text-black">
+        )
+    }
+    {/* Close the opening <div> here */}
+    {
+        user && (
+            <div className="p-10 text-center text-black">
                 <p className="text-lg font-bold">{user.firstName} {user.lastName}</p>
                 <p className="text-sm text-gray-500">{user.email}</p>
             </div>
-            )
-            }  
+        )
+    }  
             
             
-            <a href="" class="bg-white text-blue-500  px-4 rounded-lg hover:bg-blue-100 transition duration-300">
+            <a onClick = {()=>redirectToAccountPage({
+          postLoginRedirectPath: window.location.href,
+      })} class="bg-white text-blue-500 cursor-pointer  px-4 rounded-lg hover:bg-blue-100 transition duration-300">
                  Edit Profile
             </a>
         </div>
